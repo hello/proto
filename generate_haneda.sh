b839be17ec842d8234681daa25512aaa2bc70e08
Tim Bart <tim@sayhello.com>
2016-06-30 17:54:46 -0700

    More go_options

--- generate_haneda.sh
+++ generate_haneda.sh
@@ -1,20 +1,26 @@
 #!/usr/bin/env sh
 
-protoc --go_out=. async.proto streaming.proto
-protoc --go_out=. log.proto periodic.server.proto morpheus_ble.proto sync_response.proto audio_control.proto
+HANEDA_DIR=_go/haneda
+API_DIR=_go/api
 
-ROOT=/Users/tim/codebin/go/src/github.com/hello/haneda
+mkdir -p $HANEDA_DIR
+mkdir -p $API_DIR
+
+# HANEDA
+protoc --go_out=_go/haneda/ async.proto streaming.proto
+
+# API
+protoc --go_out=_go/api log.proto periodic.server.proto morpheus_ble.proto sync_response.proto audio_control.proto file_manifest.proto filetransfer.proto state.proto
+
+ROOT="$GOPATH/src/github.com/hello/haneda"
 HANEDA="$ROOT/haneda/"
 API="$ROOT/api/"
 
 echo $HANEDA
 echo $API
 
-cp async.pb.go $HANEDA
-cp streaming.pb.go $HANEDA
+cp $HANEDA_DIR/*.pb.go $HANEDA
+cp $API_DIR/*.pb.go $API
 
-cp log.pb.go $API
-cp periodic.server.pb.go $API
-cp morpheus_ble.pb.go $API
-cp sync_response.pb.go $API
-cp audio_control.pb.go $API
+rm -rf _go/
+
